bcc15e2699f4747919ade6e638ec8696152cd2cf
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-09-07 18:36:45 +0200

    feat(vaban): add vaban version 20240907

---
+++ Formula/vaban.rb
@@ -0,0 +1,20 @@
+class Vaban < Formula
+  desc "Simple and Really Fast Varnish Cache Cluster Manager (for Varnish 6.x/7.x)"
+  homepage "https://github.com/tgragnato/vaban/"
+  url "https://github.com/tgragnato/vaban.git", branch: "main"
+  version "20240907"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    ENV["GOPATH"] = buildpath
+    system "go", "build", *std_go_args, "-o", bin/"vaban", "."
+  end
+
+  test do
+    system "go", "test", "./..."
+    system bin/"vaban", "--help"
+  end
+end
