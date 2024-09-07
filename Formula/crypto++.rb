63bb8ded76f86a5f1941497768f7aa6b935a67f7
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-09-07 12:56:13 +0200

    build(crypto++): bump from the master of 2023-08-07 to v8.9.0

--- Formula/crypto++.rb
+++ Formula/crypto++.rb
@@ -1,8 +1,9 @@
 class Cryptoxx < Formula
   desc "Free C++ class library of cryptographic schemes"
   homepage "https://cryptopp.com/"
-  url "https://github.com/weidai11/cryptopp.git"
-  version "20230807"
+  url "https://github.com/weidai11/cryptopp.git",
+    tag:      "CRYPTOPP_8_9_0",
+    revision: "843d74c7c97f9e19a615b8ff3c0ca06599ca501b"
   license "BSL-1.0"
 
   def install
