931de495f08c587283717bb870186cbb3a18ec68
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-09-07 14:49:53 +0200

    fix(crypto++): the official website is not reachable

--- Formula/crypto++.rb
+++ Formula/crypto++.rb
@@ -1,6 +1,6 @@
 class Cryptoxx < Formula
   desc "Free C++ class library of cryptographic schemes"
-  homepage "https://cryptopp.com/"
+  homepage "https://github.com/weidai11/cryptopp/"
   url "https://github.com/weidai11/cryptopp.git",
     tag:      "CRYPTOPP_8_9_0",
     revision: "843d74c7c97f9e19a615b8ff3c0ca06599ca501b"
