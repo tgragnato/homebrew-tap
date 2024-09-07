aa3bc697bbfb4a4068b32d663abd8efc0808a790
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-09-07 13:11:40 +0200

    feat(magnetico): add `tgragnato/magnetico` version 1.61.2

---
+++ Formula/magnetico.rb
@@ -0,0 +1,23 @@
+class Magnetico < Formula
+  desc "Autonomous (self-hosted) BitTorrent DHT search engine suite"
+  homepage "https://github.com/tgragnato/magnetico/"
+  url "https://github.com/tgragnato/magnetico.git",
+    tag:      "v1.61.2",
+    revision: "5ed053c9c2d38c6650bc6ba22935326dc6d3b378"
+  license "AGPL-3.0-only"
+
+  depends_on "czmq" => :build
+  depends_on "go" => :build
+  depends_on "pkg-config" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "1"
+    ENV["GOPATH"] = buildpath
+    system "go", "build", *std_go_args, "-o", bin/"magnetico", "."
+  end
+
+  test do
+    system "go", "test", "./..."
+    system "#{bin}/magnetico", "--version"
+  end
+end
