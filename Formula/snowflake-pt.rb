db5762219e1c0cc4a586d141baab0747ee3f09b3
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-09-07 18:02:58 +0200

    feat(snowflake): add snowflake main 20240907

---
+++ Formula/snowflake-pt.rb
@@ -0,0 +1,22 @@
+class SnowflakePt < Formula
+  desc "WebRTC Pluggable Transport"
+  homepage "https://github.com/tgragnato/snowflake/"
+  url "https://github.com/tgragnato/snowflake.git", branch: "main"
+  version "20240907"
+  license "BSD-3-Clause"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    ENV["GOPATH"] = buildpath
+    system "go", "build", *std_go_args, "-o", bin/"snowflake_client", "./client"
+    system "go", "build", *std_go_args, "-o", bin/"snowflake_proxy", "./proxy"
+  end
+
+  test do
+    system "go", "test", "./..."
+    system bin/"snowflake_client", "--help"
+    system bin/"snowflake_proxy", "--help"
+  end
+end
