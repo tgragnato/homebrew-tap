f9e735e6b6fefeb49243bc12fc03d2ec026ded1a
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-09-09 15:05:20 +0200

    build(goflow): bump goflow from version 0.1.0 to 0.2.0

--- Formula/goflow.rb
+++ Formula/goflow.rb
@@ -2,8 +2,8 @@ class Goflow < Formula
   desc "High-scalability sFlow/NetFlow/IPFIX collector used internally at Cloudflare"
   homepage "https://github.com/tgragnato/goflow/"
   url "https://github.com/tgragnato/goflow.git",
-    tag:      "v0.1.0",
-    revision: "aa9cff987c2ba3d130e2e6779ccfd41ce0e324ac"
+    tag:      "v0.2.0",
+    revision: "0eca2c61a6d5d68da4cc0a7264773624afb93437"
   license "BSD-3-Clause"
 
   depends_on "go" => :build
