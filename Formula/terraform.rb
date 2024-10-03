9e7081dff7bbafd2c251dff9aa73073137fa4bdc
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-10-03 14:34:57 +0200

    build(terraform): bump terraform from v1.9.6 to v1.9.7

--- Formula/terraform.rb
+++ Formula/terraform.rb
@@ -2,8 +2,8 @@ class Terraform < Formula
   desc "Automate infrastructure on any cloud with Terraform"
   homepage "https://www.terraform.io/"
   url "https://github.com/hashicorp/terraform.git",
-    tag:      "v1.9.6",
-    revision: "f164d45c3f5775f930c8ccb7d8c5295d8a5fcd52"
+    tag:      "v1.9.7",
+    revision: "25c4dfba736ceba6456a57cca3703b1495a5ef62"
   license "BUSL-1.1"
 
   depends_on "go" => :build
