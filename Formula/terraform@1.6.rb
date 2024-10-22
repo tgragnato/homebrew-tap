c06754b6d4da7182d246d3386c93f51b433aefef
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-10-22 09:55:46 +0200

    feat(terraform): add terraform version 1.6.6

---
+++ Formula/terraform@1.6.rb
@@ -0,0 +1,20 @@
+class TerraformAT16 < Formula
+  desc "Automate infrastructure on any cloud with Terraform"
+  homepage "https://www.terraform.io/"
+  url "https://github.com/hashicorp/terraform.git",
+    tag:      "v1.6.6",
+    revision: "eba3240dae80b8bdd14d747cc43d056ea22573f4"
+  license "BUSL-1.1"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    ENV["GOPATH"] = buildpath
+    system "go", "build", *std_go_args, "-ldflags", "-s -w -X main.version=#{version}", "-o", bin/"terraform16", "."
+  end
+
+  test do
+    system bin/"terraform16", "--help"
+  end
+end
