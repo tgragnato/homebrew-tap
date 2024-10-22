3e3f8dd35accc64b4e6abeb01450578c2888004b
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-10-22 09:50:33 +0200

    feat(terraform): add terraform version 1.8.5

---
+++ Formula/terraform@1.8.rb
@@ -0,0 +1,20 @@
+class TerraformAT18 < Formula
+  desc "Automate infrastructure on any cloud with Terraform"
+  homepage "https://www.terraform.io/"
+  url "https://github.com/hashicorp/terraform.git",
+    tag:      "v1.8.5",
+    revision: "8f2d640b719872f9661d46deb628d1f14d0fc135"
+  license "BUSL-1.1"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    ENV["GOPATH"] = buildpath
+    system "go", "build", *std_go_args, "-ldflags", "-s -w -X main.version=#{version}", "-o", bin/"terraform18", "."
+  end
+
+  test do
+    system bin/"terraform18", "--help"
+  end
+end
