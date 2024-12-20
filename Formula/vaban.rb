49f70f82f66611ec3e09c9fa28187e435726e5a6
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-12-20 03:16:10 +0100

    build(vaban): bump vaban from 20240907 to 20241220

--- Formula/vaban.rb
+++ Formula/vaban.rb
@@ -2,7 +2,7 @@ class Vaban < Formula
   desc "Simple and Really Fast Varnish Cache Cluster Manager (for Varnish 6.x/7.x)"
   homepage "https://github.com/tgragnato/vaban/"
   url "https://github.com/tgragnato/vaban.git", branch: "main"
-  version "20240907"
+  version "20241220"
   license "MIT"
 
   depends_on "go" => :build
