979be73dc8c4f9b22636a88bd8feea780f59e9af
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2026-06-26 00:09:50 +0200

    feat(amule): change stable version to 3.0.1 and introduce HEAD

--- Formula/amule.rb
+++ Formula/amule.rb
@@ -1,9 +1,17 @@
 class Amule < Formula
   desc "'All-platform' P2P client based on eMule"
-  homepage "https://github.com/tgragnato/amule"
-  url "https://github.com/tgragnato/amule.git", branch: "main"
-  version "20250730"
+  homepage "https://github.com/amule-org/amule"
+  url "https://github.com/amule-org/amule/archive/refs/tags/3.0.1.tar.gz"
+  sha256 "29bb7694479417f55a1b1d04f3c98dff25853be14592fb0a1e49da1d93c143b3"
+  version "3.0.1"
   license "GPL-2.0"
+
+  head "https://github.com/tgragnato/amule.git", branch: "main"
+
+  stable do
+    depends_on "libmaxminddb"
+    depends_on "libupnp"
+  end
 
   depends_on "cmake" => [:build, :test]
   depends_on "gettext" => [:build, :test]
