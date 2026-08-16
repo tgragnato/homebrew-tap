8b727855701e214bcc31c64c70229a124b625156
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2026-08-16 19:54:30 +0200

    build(terraform): bump terraform from 1.15.6 to 1.15.8

--- Formula/terraform.rb
+++ Formula/terraform.rb
@@ -2,8 +2,8 @@ class Terraform < Formula
   desc "Automate infrastructure on any cloud with Terraform"
   homepage "https://github.com/hashicorp/terraform"
   url "https://github.com/hashicorp/terraform.git",
-    tag:      "v1.15.6",
-    revision: "699fc4df3a2e8f2205ec7b02698ccd76dfdc095d"
+    tag:      "v1.15.8",
+    revision: "b9e178decf87d274d25ed36bc5a4dbc857e00420"
   license "BUSL-1.1"
 
   depends_on "go" => :build
