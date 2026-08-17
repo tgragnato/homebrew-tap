46c4129b9e85c5a192f2ed4d61160078d1a0e382
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2026-08-17 13:24:25 +0200

    refactor[skip-ci]: refactor gotools test suite to validate all commands

--- Formula/gotools.rb
+++ Formula/gotools.rb
@@ -19,7 +19,19 @@ class Gotools < Formula
   end
 
   test do
-    assert_match "usage", shell_output("#{bin}/goyacc 2>&1", 2)
-    assert_match "stringer", shell_output("#{bin}/stringer 2>&1", 2)
+    exit0 = %w[
+      bundle callgraph deadcode fiximports fuzz-driver fuzz-runner
+      go-contrib-init gomvpkg goyacc html2article present ssadump stress stringer
+    ]
+    exit1 = %w[splitdwarf]
+    exit2 = %w[
+      benchcmp bisect compilebench digraph eg file2fuzz godex
+      goimports gonew gotype present2md toolstash
+    ]
+
+    (exit0 + exit1 + exit2).each { |cmd| assert_path_exists bin/cmd }
+    exit0.each { |cmd| system bin/cmd, "--help" }
+    exit1.each { |cmd| shell_output("#{bin}/#{cmd} --help 2>&1", 1) }
+    exit2.each { |cmd| shell_output("#{bin}/#{cmd} --help 2>&1", 2) }
   end
 end
