77764559c6522d8c38e9300aa4346f9172baeb35
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2026-08-17 10:53:58 +0200

    build(inca): bump inca from 1.9.0 to 1.9.4

--- Formula/inca.rb
+++ Formula/inca.rb
@@ -2,8 +2,8 @@ class Inca < Formula
   desc "API around internal and global Certificate Authority flows"
   homepage "https://github.com/immobiliare/inca/"
   url "https://github.com/immobiliare/inca.git",
-    tag:      "1.9.0",
-    revision: "0d24f1a8415517350f0347873595b659f8646183"
+    tag:      "1.9.4",
+    revision: "1c51b0d7e7c536b89ae697f440372f17d97062b7"
   license "MIT"
 
   depends_on "go" => :build
