d345f0de180bc218af493ab922162a0a4227a0b9
Tommaso Gragnato <91843630+tgragnato@users.noreply.github.com>
2026-08-23 14:52:17 +0000

    build(amule): bump amule from version 20260808 to version 20260822

--- Formula/amule.rb
+++ Formula/amule.rb
@@ -2,7 +2,7 @@ class Amule < Formula
   desc "'All-platform' P2P client based on eMule"
   homepage "https://github.com/tgragnato/amule"
   url "https://github.com/tgragnato/amule.git", branch: "main"
-  version "20260808"
+  version "20260822"
   license "GPL-2.0"
 
   livecheck do
