8ef58878c71c04e9521dffa143f3543395105d18
Tommaso Gragnato <91843630+tgragnato@users.noreply.github.com>
2026-09-06 17:18:39 +0000

    build(whisper-to-graphite): bump whisper-to-graphite from version 20260824 to version 20260831

--- Formula/whisper-to-graphite.rb
+++ Formula/whisper-to-graphite.rb
@@ -2,7 +2,7 @@ class WhisperToGraphite < Formula
   desc "Read and send metrics from whisper files to Graphite"
   homepage "https://github.com/tgragnato/whisper-to-graphite/"
   url "https://github.com/tgragnato/whisper-to-graphite.git", branch: "main"
-  version "20260824"
+  version "20260831"
   license "MIT"
 
   livecheck do
