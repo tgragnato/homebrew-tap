659261c93d34e418aeeef296af14f2735471fca0
Tommaso Gragnato <91843630+tgragnato@users.noreply.github.com>
2026-09-13 17:35:58 +0000

    build(snowflake-pt): bump snowflake-pt from version 20260904 to version 20260909

--- Formula/snowflake-pt.rb
+++ Formula/snowflake-pt.rb
@@ -2,7 +2,7 @@ class SnowflakePt < Formula
   desc "WebRTC Pluggable Transport"
   homepage "https://github.com/tgragnato/snowflake/"
   url "https://github.com/tgragnato/snowflake.git", branch: "main"
-  version "20260904"
+  version "20260909"
   license "BSD-3-Clause"
 
   livecheck do
